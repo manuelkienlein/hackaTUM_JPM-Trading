030675c48da3b732092a162249a415d22bb580cf
Jonathan <schwarzerjonathan@gmail.com>
2022-11-19 23:03:40 +0100

    moved creation of orders to /web

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from . import views
-
-
+from api.views import create
 urlpatterns = [
     path('login', views.index, name='web_index'),
     path('registration', views.registration, name='web_registration'),
+    path('offer/create', create, name = "web_createorder"),
 ]
