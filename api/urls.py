655ee362e50ad535518a2c072c0d1a0e34b78479
Manuel Kienlein <manuel.kienlein@gmx.de>
2022-11-19 12:38:17 +0100

    Added base structure for api routes

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('user/<int:userid>', views.user_index, name='api_user_index'),
+    path('user/<int:userid>/stocks', views.user_stocks, name='api_user_info'),
+    path('user/<int:userid>/stocks/buy', views.user_stocks_buy, name='api_user_stocks_buy'),
+    path('user/<int:userid>/stocks/sell', views.user_stocks_buy, name='api_user_stocks_sell'),
+    path('user/<int:userid>/stocks/delete', views.user_stocks_delete, name='api_user_stocks_delete'),
+
+    path('user/<int:userid>', views.stocks_index, name='stocks_index'),
+    path('user/<int:userid>/stocks', views.stocks_info, name='stocks_info'),
 ]
