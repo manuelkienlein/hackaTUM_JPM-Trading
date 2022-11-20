671655142cdf409dd6818f6054eba9e8c63729b2
Manuel Kienlein <manuel.kienlein@gmx.de>
2022-11-20 06:52:31 +0100

    Improved create order form

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from . import views
-from web.views import create
 
 urlpatterns = [
     path('', views.index, name='web_index'),
@@ -9,6 +8,6 @@ urlpatterns = [
     path('accounts', views.account, name='web_account'),
     path('accounts/orders', views.account_orders, name='web_account_orders'),
     path('accounts/order-history', views.account_order_history, name='web_account_order_history'),
-    path('offer/create', create, name = "web_createorder"),
-    path('accounts/controller/deleteOrder', views.account_controller_delete_order, name="web_account_controller_order_delete")
+    path('offer/create', views.account_controller_order_create, name = "web_createorder"),
+    path('accounts/controller/deleteOrder', views.account_controller_order_delete, name="web_account_controller_order_delete")
 ]
